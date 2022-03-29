23ec03cf093e1b6d48d56a44a4306aeba20c579e
kyanem <56326511+kyanem@users.noreply.github.com>
2022-03-28 23:04:22 -0600

    php

---
+++ sitioPHP/bannerAd.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/bannerAd.css">
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    
+    <div class="banner-container">
+
+        <div class="banner">
+            
+            <div class="content">
+                <span>Encuentra los</span>
+                <h3 style="color: #ff6339;">Mejores</h3>
+                <span>Restaurantes</span>
+                <!--<a href="#" class="btn">veiw offer</a>-->
+                <br>
+                <div class="boxSearch">
+                    <form>
+                        <input type="text" name="" value="Ingresa un dato...">
+                        <input type="submit" name="" value="Buscar">
+                    </form>
+                </div>
+            </div>
+            <div class="imgGallery">
+                <img class="g-img uno" src="img/platillo1.jpg" alt="">
+                <img class="g-img dos" src="img/platillo2.jpg" alt="">
+                <img class="g-img tres" src="img/platillo3.jpg" alt="">
+                <img class="g-img cuatro" src="img/platillo4.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+
+
+
+</body>
+</html>
---
+++ sitioPHP/header.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Cabecera fija</title>
+
+        <link rel="stylesheet" href="css/styles.css">
+        <!--Iconos-->
+  		<script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	
+	<header id="main-header">
+		
+		<a id="logo-header" href="#">
+			<span class="site-name">LOGO</span>
+		</a> <!-- / #logo-header -->
+
+		<nav>
+			<ul class="nav__links">
+				<li><a href="#">Restaurantes</a></li>
+				<li><a href="#">Servicios</a></li>
+				<li><a href="#">Menú</a></li>
+			</ul>
+		</nav><!-- / nav -->
+		<a href="#" class="cta"><button><i class="fa fa-user-circle" aria-hidden="true" class="userIcon"></i>Iniciar sesión</button></a>
+
+
+
+	</header><!-- / #main-header -->
+
+	
+	<section id="main-content">
+	
+		<article>
+			
+			
+			
+			<div class="content">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+			</div>
+			
+		</article> <!-- /article -->
+	
+	</section> <!-- / #main-content -->
+
+	
+	
+	<footer class="footer">
+	  	 <div class="container">
+	  	 	<div class="row">
+		        <div class="box">
+		            <figure>
+		                <a href="#">
+		                    <img src="img/logo.png" alt="Logo">
+		                </a>
+		            </figure>
+		        </div>
+
+		        <div class="box">
+		            <h4>Contacto</h4>
+		                <div class="red-social">
+		                    <a href="#" class="fa fa-facebook"></a>
+		                    <a href="#" class="fa fa-instagram"></a>
+		                    <a href="#" class="fa fa-twitter"></a>
+		                </div>
+		        </div>
+
+		  	 	<div class="footer-col">
+		            <h4>Servicio</h4>
+		            <div>
+		                <ul>
+		                    <li><a href="#">Restaurantes</a></li>
+		                    <li><a href="#">Publicidad</a></li>
+		                    <li><a href="#">Mapa</a></li>
+		                </ul>
+		            </div>
+		        </div>
+
+		        <div class="footer-col">
+		            <h4>Ayuda</h4>
+		            <div>
+		                <ul>
+		                    <li><a href="#">Administradores</a></li>
+		                    <li><a href="#">Usiarios</a></li>
+		                </ul>
+		            </div>
+		        </div>
+		  	 		
+		  	</div>
+	  	</div>
+    </footer>
+
+	
+</body>
+</html>
